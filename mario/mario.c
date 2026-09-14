ead1fcc992b545ba421df7800a9e53fb2850823b
Noble-neon <muminovsadullo777@gmail.com>
2026-09-14 19:46:13 +0500

    mario, without the spaces yet

---
+++ mario/mario.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int height = 0;
+    int sizeNow = 0;
+
+    while(height < 1){
+        printf("Height: ");
+        scanf("%i", &height);        
+    }
+
+    for(; height > 0; height --) {
+        for(int i = 0; sizeNow >= i; i++){
+            printf("#");
+        }
+        printf("\n");
+        sizeNow++;
+
+    }
+}
